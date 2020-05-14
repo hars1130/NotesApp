868eef287b27b8ffbfe0b924ae9d626ee2db77b3
hars1130 <harshavardhan.chakka@gmail.com>
2020-05-14 08:20:43 +0530

    impement note detail component

--- src/app/note-detail/note-detail.component.ts
+++ src/app/note-detail/note-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../models/note';
+import { NotesService } from '../services/notes.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-note-detail',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(public notesService: NotesService) { }
+
+  note: Note;
 
   ngOnInit() {
+    this.notesService.selectedNote.subscribe(selectedNote =>{
+      this.note = selectedNote;
+    });
+  }
+
+  onChange(){
+    this.note.timeStamp = new Date().toLocaleString();
+    this.notesService.save();
   }
 
 }
