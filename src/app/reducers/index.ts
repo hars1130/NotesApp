3efebb5d517bc4250aff9186a53209a3ed3d2e60
hars1130 <harshavardhan.chakka@gmail.com>
2020-05-14 08:16:06 +0530

    create selectors

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,33 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as fromNote from "./note.reducer";
 
-export interface State {
-
+export interface AppState {
+  notes: fromNote.NoteState;
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+export const reducers: ActionReducerMap<AppState> = {
+  notes: fromNote.reducer
 };
 
+export const getNoteState = (state: AppState) => state.notes;
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const getAllNotes = createSelector(
+  getNoteState,
+  fromNote.getNotes
+);
+
+export const getNotesBySearchString = createSelector(
+  getNoteState,
+  fromNote.getNotesBySearchString
+);
+
+export const getSelectedNote = createSelector(
+  getNoteState,
+  fromNote.getSelectedNote
+);
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+  ? []
+  : [];
